016071dba0fcce32da2ff038986903e32d1ff32e
Navyasingh37 <Singhchanda346@gmail.com>
2025-02-28 20:11:03 +0545

    Add files via upload

---
+++ percentage.py
@@ -0,0 +1,13 @@
+print("Enter Marks Obtained in 4 subjects")
+math = int(input("Math :"))
+english = int(input("English :"))
+science = int(input("Science :"))
+hindi = int(input("hindi :"))
+
+sum =math+english+science+hindi
+print("sum of amth.english,science,hindi")
+
+perc = (sum/400)*100
+
+print(end="Perncentage Mark =")
+print(perc)
